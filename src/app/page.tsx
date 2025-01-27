d635bb6c04fef3dca31d53055cf2b0061eeb767b
xop141 <lowsens400@gmail.com>
2025-01-28 00:13:53 +0800

    +header

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+  <div className="w-screen h-screen flex items-center justify-center">
+    
+  </div>
+  );
+}
